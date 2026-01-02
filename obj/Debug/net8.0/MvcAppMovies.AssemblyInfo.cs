008ed6f5ca7251b84520d8fcc1f6775708382217
Patryk Figas <patrykf09@wp.pl>
2026-01-02 20:14:39 +0100

    Dokumentacja

--- obj/Debug/net8.0/MvcAppMovies.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcAppMovies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcAppMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b97f836e381bc3264fdb86ca976b86b656714f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea26e9229cc837b6a3a87b02b3c24372409e6e58")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcAppMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcAppMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
